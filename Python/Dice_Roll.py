d4e7c6401d1375c369d5270ee3a519a1a31cc6f7
Corong0 <augustocunhaclaro@outlook.com>
2025-05-25 12:41:51 -0300

    first commit

---
+++ Python/Dice_Roll.py
@@ -0,0 +1,52 @@
+import random
+
+MAXN = 100
+MINN = 0
+
+MAX_ROLLS = 3
+MIN_ROLLS = 1
+
+
+def check_sides():
+    while True:
+        sides = input(f"Insira o número de lados entre {MINN + 1} e {MAXN}: ")
+        try:
+            sides = int(sides)
+            if sides > MINN and sides <= MAXN:
+                return sides
+            else:
+                print(
+                    f"O número deve ser maior que {MINN} e menor ou igual a {MAXN}.")
+        except ValueError:
+            print("Por favor, insira um número válido.")
+
+
+cube_sides = check_sides()
+print(f"Número de lados: {cube_sides}")
+
+
+def check_rolls():
+    while True:
+        try:
+            rolls = input(
+                f"Insira o número de dados a serem jogados entre ({MIN_ROLLS} - {MAX_ROLLS}): ") # EU CONSEGUIA(ISDASDHAHAAHAHAHAHAHHASDijhdsfiuvhduvhedfruyfgb)
+            rolls = int(rolls)
+            if rolls > MIN_ROLLS and rolls <= MAX_ROLLS:
+                return rolls
+            else:
+                print(
+                    f"O número deve ser maior que {MIN_ROLLS} e menor ou igual a {MAX_ROLLS}")
+        except ValueError:
+            print("Por favor, insira um número válido.")
+
+
+roll_count = check_rolls()
+print(f"O número de roladas é: {roll_count}.")
+
+
+def dice_roll():
+    rolls = [random.randint(1, cube_sides) for _ in range(roll_count)]
+    print(f"Resultados dos {roll_count} dados de {cube_sides} lados: {rolls}")
+
+
+dice_roll()
---
+++ Python/greet_dem.py
@@ -0,0 +1,10 @@
+x = input("Input your first name: ")
+y = input("Input your last name: ")
+
+
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+greet(x, y) #Check how cool this is nigga
